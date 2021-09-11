034efc4a1ff87c5a4c525e327b0332978a24990f
Alan Kuriakose <aln0071@gmail.com>
2021-09-11 00:50:53 -0700

    initialization: create basic layout

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const dotenv = require('dotenv');
+
+const { PORT, CSS_PREFIX } = dotenv.config().parsed;
+const { NODE_ENV } = process.env;
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'}, 
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: `${CSS_PREFIX}[local]`,
+                exportLocalsConvention: 'camelCase',
+              },
+            },
+          },
+          { loader: 'sass-loader' },
+        ],
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx", ".scss"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    port: PORT,
+    hot: true,
+    liveReload: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html'
+    })
+  ]
+};
