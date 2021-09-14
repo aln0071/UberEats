5675dbfa16449b37a49a608c843bf295abbdbef8
Alan Kuriakose <aln0071@gmail.com>
2021-09-14 16:32:57 -0700

    FrontEnd: make sample api call

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <h1> Hello, World! </h1>
-  </div>
-);
+const getApi = () => {
+  const url = 'http://localhost:3001';
+  fetch(url)
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const App = () => {
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1> Hello, World! </h1>
+    </div>
+  );
+};
 
 export default App;
