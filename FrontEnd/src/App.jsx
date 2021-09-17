0231717231edf927eed33174834a7c12723a26d9
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-09-16 17:55:52 -0700

    login-page: remove context

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,35 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import Routes from './Routes';
 import 'react-toastify/dist/ReactToastify.css';
-import UserContext from './utils/usercontext';
 
-const App = () => {
-  const [userDetails, setUserDetails] = useState(
-    JSON.parse(window.sessionStorage.getItem('userDetails')) || {},
-  );
-  const value = { userDetails, setUserDetails };
-
-  return (
-    <div className="App">
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnVisibilityChange
-        draggable
-        pauseOnHover
-      />
-      <ToastContainer />
-      <UserContext.Provider value={value}>
-        <Routes />
-      </UserContext.Provider>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnVisibilityChange
+      draggable
+      pauseOnHover
+    />
+    <ToastContainer />
+    <Routes />
+  </div>
+);
 
 export default App;
--- FrontEnd/src/utils/usercontext.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const UserContext = React.createContext({
-  userDetails: JSON.parse(window.sessionStorage.getItem('userDetails')) || {},
-  setUserDetails: () => {},
-});
-
-export default UserContext;
