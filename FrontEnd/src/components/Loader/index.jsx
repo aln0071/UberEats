b3e5a95436e71e4caa70f0f0788fa9de1254aac8
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-09-22 11:39:06 -0700

    FrontEnd: add loader

---
+++ FrontEnd/src/components/Loader/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="lds-spinner">
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </div>
+  );
+}
