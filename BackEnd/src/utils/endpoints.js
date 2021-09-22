44c566679f58af98a25d8f8fb4c9de5e1b30c735
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-09-21 17:48:20 -0700

    BackEnd: Encrypt password

--- BackEnd/src/utils/endpoints.js
+++ BackEnd/src/utils/endpoints.js
@@ -1,29 +1,43 @@
+const bcrypt = require('bcrypt');
 const { executeQuery } = require('./utils');
 const { _login, _registerUser } = require('./queries');
 
 function login(username, password) {
-  return executeQuery(_login, { username, password }).then((response) => {
+  return executeQuery(_login, { email: username }).then(async (response) => {
     if (response.length === 0) {
       throw new Error('Invalid credentials');
+    } else {
+      const userData = response[0];
+      const isValidUser = await bcrypt.compare(password, userData.password);
+      if (isValidUser) {
+        delete userData.password;
+        return userData;
+      }
+      throw new Error('Invalid credentials');
     }
-    return {
-      userdata: response[0],
-    };
   });
 }
 
-function register(params) {
+async function register(params) {
   const {
     email, password, type, name,
   } = params;
+
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
   if (type === 'customer') {
-    return executeQuery(_registerUser, { email, password, name }).catch(
-      (error) => {
-        if (String(error.message).startsWith('ER_DUP_ENTRY')) {
-          throw new Error('User with this email already exists');
-        }
-      },
-    );
+    return executeQuery(_registerUser, {
+      email,
+      password: hashedPassword,
+      name,
+    }).catch((error) => {
+      if (String(error.message).startsWith('ER_DUP_ENTRY')) {
+        throw new Error('User with this email already exists');
+      } else {
+        throw error;
+      }
+    });
   }
   return null;
 }
--- BackEnd/src/utils/queries.js
+++ BackEnd/src/utils/queries.js
@@ -1,6 +1,5 @@
 module.exports = {
-  _login:
-    'select * from users where username = :username and password = :password',
+  _login: 'select * from users where email = :email',
   _registerUser:
     'insert into users(email, password, type, name) values( :email, :password, "c", :name )',
 };
