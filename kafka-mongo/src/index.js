93c3799a4aaf15da854a14239f70785f52b63043
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-10-20 10:09:14 -0700

    Kafka: Modify folder structure

--- kafka-mongo/src/index.js
+++ kafka-mongo/src/index.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
 async function test() {
-    await mongoose.connect('mongodb+srv://ubereats.sqgqq.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        ssl: true,
-        sslKey: '../cert.pem', 
-        sslCert: '../cert.pem'
-    })
-    const Cat = mongoose.model('Cat', { name: String });
+  await mongoose.connect(
+    'mongodb+srv://ubereats.sqgqq.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      ssl: true,
+      sslKey: 'cert.pem',
+      sslCert: 'cert.pem',
+    },
+  );
+  const Cat = mongoose.model('Cat', { name: String });
 
-    const kitty = new Cat({ name: 'Zildjian' });
-    kitty.save().then(() => console.log('meow'));
+  const kitty = new Cat({ name: 'Zildjian' });
+  kitty.save().then(() => console.log('meow'));
 }
 
 test();
