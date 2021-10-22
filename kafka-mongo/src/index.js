b4954ff4821fd42648e5193319bbf8588a065666
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-10-22 10:41:05 -0700

    Mongo: Clean code

--- kafka-mongo/src/index.js
+++ kafka-mongo/src/index.js
@@ -39,25 +39,6 @@ const User = require('./models/UserModel');
 
 const userService = require('./services/user');
 
-// async function test() {
-//   await mongoose.connect(
-//     'mongodb+srv://ubereats.sqgqq.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority',
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       ssl: true,
-//       sslKey: 'cert.pem',
-//       sslCert: 'cert.pem',
-//     },
-//   );
-//   const Cat = mongoose.model('Cat', { name: String });
-
-//   const kitty = new Cat({ name: 'Zildjian' });
-//   kitty.save().then(() => console.log('meow'));
-// }
-
-// test();
-
 // hi message
 app.get('/', async (req, res) => {
   try {
@@ -93,22 +74,3 @@ const server = app.listen(3002, () => {
 });
 
 module.exports = server;
-
-// const client = new MongoClient('mongodb+srv://ubereats.sqgqq.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority', {
-//   sslKey: credentials,
-//   sslCert: credentials
-// });
-// async function run() {
-//   try {
-//     await client.connect();
-//     const database = client.db("testDB");
-//     const collection = database.collection("testCol");
-//     const docCount = await collection.countDocuments({});
-//     console.log(docCount);
-//     // perform actions using client
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
