f52abca2177cd07e6642389a3b402fe82af640df
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-11-03 11:48:47 -0700

    Mongo: Update required fields in add dish model

--- kafka-mongo/src/models/DishModel.js
+++ kafka-mongo/src/models/DishModel.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const dishSchema = new Schema({
-  restaurantid: { type: mongoose.Types.ObjectId },
-  dishname: String,
+  restaurantid: { type: mongoose.Types.ObjectId, required: true },
+  dishname: { type: String, required: true },
   description: String,
-  category: Number,
-  price: Number,
+  category: { type: Number, required: true },
+  price: { type: Number, required: true },
   pictures: Array,
 });
 
