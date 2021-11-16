8b343b0e0014828ad1d61cbd94374df6b677c67f
Alan Kuriakose <alan.kuriakose@sjsu.edu>
2021-11-16 10:14:03 -0800

    Lab3: Update kafka setup script

--- kafka-mongo/kafkaSetup.sh
+++ kafka-mongo/kafkaSetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for TOPIC in location_topic user_topic dish_topic restaurant_topic response_topic; do
-  /home/alan/Softwares/Softwares/kafka_2.13-3.0.0/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic $TOPIC
+  /home/alan/Softwares/Softwares/kafka_2.13-3.0.0/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic $TOPIC &
 done
+wait
